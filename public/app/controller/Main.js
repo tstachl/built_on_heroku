f6f9d8e674f363abd0c6ef5266407769dc75acf9
Thomas Stachl <thomas@stachl.me>
2012-05-29 14:01:42 +0200

    Added a url parser so any url can be used and it will always use the hostname of the url

--- public/app/controller/Main.js
+++ public/app/controller/Main.js
@@ -11,66 +11,77 @@ Ext.define('BuiltOnHeroku.controller.Main', {
   
   doCheck: function() {
     Ext.Viewport.setMasked(true);
-    Ext.Ajax.request({
-      url: '/',
-      method: 'POST',
-      params: {
-        domain: Ext.getCmp('domain').getValue()
-      },
-      success: function(r){
-        r = Ext.decode(r.responseText);
-        Ext.Viewport.unmask();
-        var html;
-        if (r.success === true) {
-          html = [
-            '<h1>Hooray!</h1>',
-            '<p>The domain (' + Ext.getCmp('domain').getValue() + ') is providing ',
-            'you with an application built on <a href="http://heroku.com">Heroku</a>.</p>',
-            '<p>Give it a try and build your own app ...</p>'
-          ];
-        } else if (r.success === false) {
-          html = [
-            '<h1>Too bad!</h1>',
-            '<p>This domain (' + Ext.getCmp('domain').getValue() + ') doesn\'t seem to be ',
-            'providing an application built on <a href="http://heroku.com">Heroku</a>.</p>',
-            '<p>Why don\'t you give it a go and build one ...</p>'
-          ];
-        } else {
-          html = [
-            '<h1>Sorry!</h1>',
-            '<p>Something went wrong ...</p>',
-            '<p>We are unable to provide you with a decisive answer ',
-            'about this domain (' + Ext.getCmp('domain').getValue() + ') at the moment.</p>',
-            '<p>Why don\'t you try it again later ...</p>'
-          ];
-        }
-        
-        Ext.Viewport.add({
-          xtype: 'panel',
-          modal: true,
-          hideOnMaskTap: true,
-          showAnimation: {
-            type: 'popIn',
-            duration: 250,
-            easing: 'ease-out'
-          },
-          hideAnimation: {
-            type: 'popOut',
-            duration: 250,
-            easing: 'ease-out'
-          },
-          centered: true,
-          width: Ext.os.deviceType == 'Phone' ? 260 : 400,
-          height: Ext.os.deviceType == 'Phone' ? 220 : 400,
-          styleHtmlContent: true,
-          html: html.join(''),
-          items: {
-            docked: 'top',
-            xtype: 'toolbar',
-            title: Ext.getCmp('domain').getValue()
+    var a      = document.createElement('a'),
+        domain = Ext.getCmp('domain').getValue();
+    
+    if (domain.indexOf('http') !== 0) {
+      domain = 'http://' + domain;
+    }
+    a.href = domain;
+    
+    if (a.hostname) {
+      Ext.Ajax.request({
+        url: '/',
+        method: 'POST',
+        params: {
+          domain: a.hostname
+        },
+        success: function(r){
+          delete a;
+          r = Ext.decode(r.responseText);
+          Ext.Viewport.unmask();
+          var html;
+          if (r.success === true) {
+            html = [
+              '<h1>Hooray!</h1>',
+              '<p>The domain (' + Ext.getCmp('domain').getValue() + ') is providing ',
+              'you with an application built on <a href="http://heroku.com">Heroku</a>.</p>',
+              '<p>Give it a try and build your own app ...</p>'
+            ];
+          } else if (r.success === false) {
+            html = [
+              '<h1>Too bad!</h1>',
+              '<p>This domain (' + Ext.getCmp('domain').getValue() + ') doesn\'t seem to be ',
+              'providing an application built on <a href="http://heroku.com">Heroku</a>.</p>',
+              '<p>Why don\'t you give it a go and build one ...</p>'
+            ];
+          } else {
+            html = [
+              '<h1>Sorry!</h1>',
+              '<p>Something went wrong ...</p>',
+              '<p>We are unable to provide you with a decisive answer ',
+              'about this domain (' + Ext.getCmp('domain').getValue() + ') at the moment.</p>',
+              '<p>Why don\'t you try it again later ...</p>'
+            ];
           }
-        });
-      }
-    });
+          
+          Ext.Viewport.add({
+            xtype: 'panel',
+            modal: true,
+            hideOnMaskTap: true,
+            showAnimation: {
+              type: 'popIn',
+              duration: 250,
+              easing: 'ease-out'
+            },
+            hideAnimation: {
+              type: 'popOut',
+              duration: 250,
+              easing: 'ease-out'
+            },
+            centered: true,
+            width: Ext.os.deviceType == 'Phone' ? 260 : 400,
+            height: Ext.os.deviceType == 'Phone' ? 220 : 400,
+            styleHtmlContent: true,
+            html: html.join(''),
+            items: {
+              docked: 'top',
+              xtype: 'toolbar',
+              title: Ext.getCmp('domain').getValue()
+            }
+          });
+        }
+      });
+    }
   }
 });
